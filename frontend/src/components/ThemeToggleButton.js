32c6e37d9a7f852c69af6bc2fcbff663656a2235
theanzy <theanzy@DESKTOP-2NQ3FQ7>
2022-02-08 21:45:28 +0800

    slider edit

--- frontend/src/components/ThemeToggleButton.js
+++ frontend/src/components/ThemeToggleButton.js
@@ -1,18 +1,9 @@
 const ThemeToggleButton = ({ onToggle }) => {
   return (
-    <div>
-      <input
-        type='checkbox'
-        className='checkbox'
-        id='checkbox'
-        onChange={onToggle}
-      />
-      <label htmlFor='checkbox' className='label'>
-        <i className='moon'> x </i>
-        <i className='sun'> x </i>
-        <div className='ball' />
-      </label>
-    </div>
+    <label className='switch'>
+      <input type='checkbox' onChange={onToggle} />
+      <span className='slider round'></span>
+    </label>
   );
 };
 
